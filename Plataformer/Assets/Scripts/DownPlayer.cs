1889188abeb9e024c5ccffecdf6aedb82ddeb81e
Wemir <dante_ro@hotmail.com.ar>
2017-08-30 22:35:17 -0300

    Prueba

--- Plataformer/Assets/Scripts/DownPlayer.cs
+++ Plataformer/Assets/Scripts/DownPlayer.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DownPlayer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        enabled = false;
+    Vector3 scalePlayer;
+	void Start ()
+    {
+        scalePlayer = transform.localScale;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetButton("Down"))
+        {
+            transform.localScale = Vector3.one;
+        }
+        if (Input.GetButtonUp("Down"))
+        {
+            transform.localScale = scalePlayer;
+        }
 	}
 }
--- Plataformer/Assets/Scripts/MovPlayer.cs
+++ Plataformer/Assets/Scripts/MovPlayer.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class MovPlayer : MonoBehaviour {
     private Rigidbody2D rgb;
-    private JumpPlayer jumpState;
-    private DownPlayer downPlayer;
     private bool right;
     private bool left;
     private bool jump;
-    private bool down;
     private float vel;
     private float velJump;
 
 	void Awake () {
         rgb = GetComponent<Rigidbody2D>();
         right = false;
-        vel = 5;
-        velJump = 50;
+        left = false;
+        jump = false;
+        vel = 10;
+        velJump = 200;
 	}
 
     void FixedUpdate()
@@ -28,17 +27,11 @@ public class MovPlayer : MonoBehaviour {
             rgb.AddRelativeForce(Vector3.left * vel, ForceMode2D.Impulse);
         if (jump)
             rgb.AddRelativeForce(Vector3.up * velJump, ForceMode2D.Impulse);
-        if (down)
-            rgb.AddRelativeForce(Vector3.right * vel, ForceMode2D.Impulse);
     }
     void Update ()
     {
         right = Input.GetButton("Right");
         left = Input.GetButton("Left");
-        jump = Input.GetButtonUp("Jump");
-        down = Input.GetButton("Down");
-        
-        
-        
+        jump = Input.GetButtonDown("Jump");          
 	}
 }
