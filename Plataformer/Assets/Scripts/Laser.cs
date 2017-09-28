111c6e16fef5c861e77a47cf040b6c2c73431c15
Rodrigo <dante_ro@hotmail.com.ar>
2017-09-27 22:11:36 -0300

    Primer Prototipo

---
+++ Plataformer/Assets/Scripts/Laser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+    [SerializeField]
+    private Vector3 scaleValue;
+    private float timer;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            print("asd");
+            transform.localScale = scaleValue;
+            timer = 0;
+        }
+        else
+            transform.localScale = Vector3.one;
+	}
+}
